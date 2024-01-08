a1ff728c549df415994f10167f30d1c4141b9c11
Mingyang <141198127+MingyangXiaKira@users.noreply.github.com>
2024-01-08 13:22:08 +0000

    Update CategoriesApis.js

--- client/src/Redux/API/CategoriesApis.js
+++ client/src/Redux/API/CategoriesApis.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 // ************ PUBLIC APIs ************
 const baseUrl = process.env.REACT_APP_API_URL;
-
 // get all categories
 const getCategoriesService = async () => {
   const { data } = await axios.get(`${baseUrl}/api/categories`);
